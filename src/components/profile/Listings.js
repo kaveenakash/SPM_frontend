f6e7ee7bb9e518d6fa84293d02d47f52f22f7d0f
kaveenakashya <kaveenakashya@gmail.com>
2021-10-03 03:06:19 +0530

    history card base added

--- src/components/profile/Listings.js
+++ src/components/profile/Listings.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Container from '@material-ui/core/Container'
 
 export default function Listings() {
+    const classes = useStyles()
     return (
-        <div>
-            fsfs
-        </div>
+        <Container>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            dasfdsf
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      marginTop: "4rem",
+      minWidth: 950,
+     
+    },
+    
+  }));
