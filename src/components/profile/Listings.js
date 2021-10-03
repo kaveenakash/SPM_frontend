7e209f7d3c17e3764b70528bf26a276bbe8e6f1e
kaveenakashya <kaveenakashya@gmail.com>
2021-10-03 19:55:17 +0530

    fixed issue

--- src/components/profile/Listings.js
+++ src/components/profile/Listings.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -6,111 +6,147 @@ import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import DeleteIcon from '@material-ui/icons/Delete';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from "@material-ui/icons/Delete";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import EditIcon from "@material-ui/icons/Edit";
 import DeleteDialog from "./DeleteDialog";
 import ViewDialog from "./ViewDialog";
-import axios from '../axios/axios'
-import {useSelector} from 'react-redux'
+import axios from "../axios/axios";
+import { useSelector } from "react-redux";
 
 export default function Listings(props) {
   const classes = useStyles();
-  const [openDelete,setOpenDelete] = useState(false)
-  const [openView,setOpenView] = useState(false)
-  const [userListings,setUserListings] = useState([])
+  const [openDelete, setOpenDelete] = useState(false);
+  const [openView, setOpenView] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
-  const userId = localStorage.getItem('userId')
-  const handleDelete = () =>{
-      setOpenDelete(!openDelete)
-  }
-  const handleView = () =>{
-    setOpenView(!openView)
-  }
+  const userId = localStorage.getItem("userId");
+  const handleDelete = () => {
+    setOpenDelete(!openDelete);
+  };
+  const handleView = () => {
+    setOpenView(!openView);
+  };
 
   useEffect(() => {
-    getUserListings()
-  }, [])
+    getUserListings();
+  }, []);
 
-
-
-  const getUserListings = async() =>{
-    const data = {userId:userId}
-    const result = await axios.post('auth/get-user-listings',data)
-    if(result.data.propertyListings){
-      
-      props.handleTotalListings(result.data.propertyListings.length)
+  const getUserListings = async () => {
+    const data = { userId: userId };
+    const result = await axios.post("auth/get-user-listings", data);
+    if (result.data.propertyListings) {
+      props.handleTotalListings(
+        result.data.propertyListings.length + result.data.vehicleListings.length
+      );
     }
-    setUserListings(result.propertyListings)
-    console.log(result)
-  }
-
+    setUserListings([...result.data.propertyListings]);
+    // setUserListings([...result.data.propertyListings,...result.data.vehicleListings])
+    console.log(result);
+  };
+  console.log(userListings);
   const imageUrl =
     "http://patpat-s3-live.s3.amazonaws.com/uploads/30ab9e94924fbbd7efa6682bbce08a29-710100.jpeg";
   return (
     <Container>
-      <Grid container alignItems="center" direction="column" >
-        <Grid item>
-          <Card className={classes.root}>
-            <CardContent>
-              <Grid container item justifyContent="space-between"> 
-                <Grid item>
-                  <Grid item container spacing={4}>
-                    <Grid item>
-                      <img
-                        alt="Remy Sharp"
-                        width="150rem"
-                        src={imageUrl}
-                        className={classes.large}
-                      />
-                    </Grid>
-                    <Grid item>
-                        <Grid item container direction="column">
-                            <Grid item>
-                            <Typography variant="subtitle1"> <li>Title - Land Sale in Malabe</li></Typography>
-                            </Grid>
-                            <Grid item>
-                            <Typography variant="subtitle1"><li>Price - Rs.45,0000</li></Typography>
-                            </Grid>
-                            <Grid item>
-                            <Typography variant="subtitle1"><li>Type - Land</li></Typography>
-                            </Grid>
-                            <Grid item>
-                            <Typography variant="subtitle1"><li>Location - Malabe</li></Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item >
-                  <Grid item container direction="column" justifyContent="center" alignItems="center">
-                      <br/>
-                      <br/>
-                      
+      <Grid container alignItems="center" direction="column">
+        {userListings.map((item) => {
+          if (item.listingType == "property") {
+            return (
+              <Grid item>
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Grid container item justifyContent="space-between">
                       <Grid item>
-                          <Grid item container spacing={2}>
+                        <Grid item container spacing={4}>
+                          <Grid item>
+                            <img
+                              alt="Remy Sharp"
+                              width="150rem"
+                              src={item.images && item.images[0]}
+                              className={classes.large}
+                            />
+                          </Grid>
+                          <Grid item>
+                            <Grid item container direction="column">
+                              <Grid item>
+                                <Typography variant="subtitle1">
+                                  {" "}
+                                  <li>Title - {item.title}</li>
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Typography variant="subtitle1">
+                                  <li>Price - Rs.{item.price.toLocaleString("en-US")}</li>
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Typography variant="subtitle1">
+                                  <li>Type - {item.propertyType}</li>
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Typography variant="subtitle1">
+                                  <li>Location - {item.area}</li>
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid
+                          item
+                          container
+                          direction="column"
+                          justifyContent="center"
+                          alignItems="center"
+                        >
+                          <br />
+                          <br />
+
+                          <Grid item>
+                            <Grid item container spacing={2}>
                               {/* <Grid item>
                                   <EditIcon fontSize="medium" className={classes.editBtn}/>
-                              </Grid> */}
+                                </Grid> */}
                               <Grid item>
-                                  <DeleteIcon fontSize="medium" className={classes.deleteBtn} onClick={() =>handleDelete() }/>
+                                <DeleteIcon
+                                  fontSize="medium"
+                                  className={classes.deleteBtn}
+                                  onClick={() => handleDelete()}
+                                />
                               </Grid>
                               <Grid item>
-                                  <VisibilityIcon fontSize="medium" className={classes.viewBtn} onClick={() =>handleView() }/>
+                                <VisibilityIcon
+                                  fontSize="medium"
+                                  className={classes.viewBtn}
+                                  onClick={() => handleView()}
+                                />
                               </Grid>
+                            </Grid>
                           </Grid>
+                        </Grid>
                       </Grid>
-                  </Grid>
-                </Grid>
-               
+                    </Grid>
+                  </CardContent>
+                </Card>
               </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-       
+            );
+          } else {
+          }
+        })}
       </Grid>
-      {openDelete && <DeleteDialog open={openDelete} handleDelete={handleDelete}/>}
-      {openView && <ViewDialog open={openView} title={"Listing Details"} handleView={handleView}/>}
+      {openDelete && (
+        <DeleteDialog open={openDelete} handleDelete={handleDelete} />
+      )}
+      {openView && (
+        <ViewDialog
+          open={openView}
+          title={"Listing Details"}
+          handleView={handleView}
+        />
+      )}
     </Container>
   );
 }
@@ -121,20 +157,19 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 850,
     backgroundColor: "#FFFDE7",
   },
-  editBtn:{
+  editBtn: {
     "&:hover": {
-        color: theme.palette.success.main
-      }
+      color: theme.palette.success.main,
+    },
   },
-  deleteBtn:{
+  deleteBtn: {
     "&:hover": {
-        color: theme.palette.error.main
-      }
+      color: theme.palette.error.main,
+    },
   },
-  viewBtn:{
+  viewBtn: {
     "&:hover": {
-        color: theme.palette.primary.main
-      }
+      color: theme.palette.primary.main,
+    },
   },
-
 }));
--- src/components/profile/ViewDialog.js
+++ src/components/profile/ViewDialog.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function ViewDialog(props) {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -26,19 +28,25 @@ export default function ViewDialog(props) {
       <Dialog
         fullScreen={fullScreen}
         open={props.open}
-       
         // onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
-        <center><DialogTitle id="responsive-dialog-title">{props.title}</DialogTitle></center>
-        <DialogContent  style={{minWidth:400}}>
-          <DialogContentText>
-            Land Sale In Malabe
-          </DialogContentText>
-          <DialogContentText>
-            Land Sale In Malabe
-          </DialogContentText>
-        </DialogContent>
+        <center>
+          <DialogTitle id="responsive-dialog-title">{props.title}</DialogTitle>
+        </center>
+        <center>
+          {" "}
+          <img
+            alt="Remy Sharp"
+            width="150rem"
+            src={'http://patpat-s3-live.s3.amazonaws.com/uploads/30ab9e94924fbbd7efa6682bbce08a29-710100.jpeg'}
+            className={classes.large}
+          />
+        </center>
+        <center> <DialogContent style={{ minWidth: 400 }}>
+          <DialogContentText>Land Sale In Malabe</DialogContentText>
+          <DialogContentText>Land Sale In Malabe</DialogContentText>
+        </DialogContent></center>
         <DialogActions>
           <Button autoFocus onClick={() => props.handleView()} color="warning">
             Cancel
@@ -48,3 +56,31 @@ export default function ViewDialog(props) {
     </div>
   );
 }
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "4rem",
+    minWidth: 850,
+    backgroundColor: "#FFFDE7",
+  },
+  editBtn: {
+    "&:hover": {
+      color: theme.palette.success.main,
+    },
+  },
+  deleteBtn: {
+    "&:hover": {
+      color: theme.palette.error.main,
+    },
+  },
+  viewBtn: {
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
+  large: {
+    width: theme.spacing(35),
+    height: theme.spacing(20),
+  },
+}));
